568b1c007541d84ea8050cf781428fbddfd16e8c
Arnaud <arnaud@MacBook-Pro-de-Arnaud.local>
2019-02-10 11:47:37 +0100

    Ajout de la traduction dans le mail

--- src/P4/MuseumBundle/Service/MailService.php
+++ src/P4/MuseumBundle/Service/MailService.php
@@ -7,33 +7,40 @@ use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\RouterInterface;
-use Symfony\Component\Templating\EngineInterface;
+use Symfony\Component\Translation\TranslatorInterface;
 
 class MailService
 {
 	private $mailer;
 	private $twig;
+	private $translator;
 
-	public function __construct(\Swift_Mailer $mailer, \Twig_Environment $twig)
+	public function __construct(\Swift_Mailer $mailer, \Twig_Environment $twig, TranslatorInterface $translator)
 	{
 		$this->mailer = $mailer;
 		$this->twig = $twig;
+		$this->translator = $translator;
 	}
 
-
-	public function sendMessage(Orders $order)
+	public function sendConfirmMessage(Orders $order)
 	{	
-		$message = (new \Swift_Message('Votre commande'));
 		$mail = $order->getCustomer()->getMail();
 		$listtickets = $order->getTickets();
+		
+		$message = $this->createMessage($this->translator->trans('order.title'), 'musee@louvre.com', $mail, $order, 'P4MuseumBundle:Ticket:mail.html.twig');
+		$this->mailer->send($message);	
+	}
 
+	public function createMessage($subject, $from, $to, $order, $twig)
+	{
+		$message = (new \Swift_Message($subject));
 		$message
-			->setFrom('musee@louvre.com')
-			->setTo($mail)
-			->setBody($this->twig->render('P4MuseumBundle:Ticket:mail.html.twig', array('mail' => $mail,
-		                                                                                'orders' => $order,
-		                                                                            	'listtickets' => $listtickets)),
+			->setFrom($from)
+			->setTo($to)
+			->setBody($this->twig->render($twig, array('mail' => $order->getCustomer()->getMail(),
+		                                               'orders' => $order,
+		                                               'listtickets' => $order->getTickets())),
 		                'text/html');
-		$this->mailer->send($message);
+		return $message;
 	}
 }
