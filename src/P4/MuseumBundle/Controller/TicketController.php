3c3a98db60a49ff5f6ee8a02c0eb05f09adaea3c
Arnaud <arnaud@MacBook-Pro-de-Arnaud.local>
2019-02-10 10:40:55 +0100

    Intégration du Mailservice dans le contrôleur

--- src/P4/MuseumBundle/Controller/TicketController.php
+++ src/P4/MuseumBundle/Controller/TicketController.php
@@ -6,13 +6,10 @@ namespace P4\MuseumBundle\Controller;
 
 use P4\MuseumBundle\Service\TicketService;
 use P4\MuseumBundle\Service\StripeService;
+use P4\MuseumBundle\Service\MailService;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
-use Symfony\Component\Validator\Constraints\DateTime;
-use P4\MuseumBundle\Entity\Ticketowner;
-use P4\MuseumBundle\Entity\Ticket;
-use P4\MuseumBundle\Entity\Customer;
 use P4\MuseumBundle\Entity\Orders;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use P4\MuseumBundle\Form\OrdersType;
@@ -28,11 +25,13 @@ class TicketController extends Controller
     }
     
     public function checkoutAction(Request $request)
-    {// Condition affichage -> Erreur 404 si id = null
+    {
+        // Condition affichage -> Erreur 404 si id = null ou totalprice = 0
         $session = $request->getSession();
         $id = $session->get('orderid');
-     
-        if(!$id) {
+        $totalprice = $session->get('totalprice');
+
+        if(!$id || $totalprice == 0) {
             throw new NotFoundHttpException();     
         }
         else {
@@ -43,7 +42,7 @@ class TicketController extends Controller
                 }
             }
 
-            return $this->render('P4MuseumBundle:Ticket:checkout.html.twig');
+        return $this->render('P4MuseumBundle:Ticket:checkout.html.twig');
     }
 
     public function buyAction(Request $request)
@@ -53,16 +52,14 @@ class TicketController extends Controller
         // On crée le FormBuilder grâce au service form factory
         $form = $this->createForm(OrdersType::class, $order);
 
-        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) 
-            {
+        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
                 $ticketService = $this->get('museum.ticket');
                 $ticketService->createTicket($order);
               //Redirection vers le récapitulatif
                 return $this->redirectToRoute('p4_museum_recap');
-            }
+        }
 
-        else
-        {
+        else {
                 $ticketService = $this->get('museum.ticket');
                 $ticketService->removeOrders($order);
         }
@@ -73,35 +70,45 @@ class TicketController extends Controller
     }
 
     public function recapAction(Request $request)
-        {//Condition affichage : Erreur 404 si id = null
-            $session = $request->getSession();
-            $id = $session->get('orderid');
+    {
+        //Condition affichage : Erreur 404 si id = null
+        $session = $request->getSession();
+        $id = $session->get('orderid');
 
-            if(!$id) {
-                throw new NotFoundHttpException();     
+        if(!$id) {
+            throw new NotFoundHttpException();     
+        }
+        else {
+            $em = $this->getDoctrine()->getManager();
+            $order = $em->getRepository('P4MuseumBundle:Orders')->find($id);
+            // Nombre de tickets par commande
+            $numberoftickets = $em->getRepository('P4MuseumBundle:Ticket')->countByOrder($id);
+            $session->set('numberoftickets', $numberoftickets);
+            // Liste des tickets de la commande
+            $listtickets = $order->getTickets();
+            // Prix total
+            $totalprice = $order->getTotalprice();
+            $session->set('totalprice', $totalprice);
+
+            if($numberoftickets > 0) {
+                return $this->render('P4MuseumBundle:Ticket:recap.html.twig', array(
+                    'orders' => $order,
+                    'listtickets' => $listtickets,
+                    'numberoftickets' => $numberoftickets));
             }
             else {
-                $em = $this->getDoctrine()->getManager();
-                $order = $em->getRepository('P4MuseumBundle:Orders')->find($id);
-                $numberoftickets = $em->getRepository('P4MuseumBundle:Ticket')->countByOrder($id);
-                $listtickets = $order->getTickets();
-                $totalprice = $order->getTotalprice();
-                $session->set('totalprice', $totalprice);
-            
-            return $this->render('P4MuseumBundle:Ticket:recap.html.twig', array(
-                'orders' => $order,
-                'listtickets' => $listtickets,
-                'numberoftickets' => $numberoftickets));
+                throw new NotFoundHttpException();   
             }
         }
+    }
 
-    public function deleteAction(Request $request)
+    public function deleteAction(Request $request) // Suppression d'un billet dans la récap
     {
         $session = $request->getSession();
 
-        $orderid = $session->get('orderid'); // id de la commande
+        $id = $session->get('orderid'); // id de la commande
         $em = $this->getDoctrine()->getManager();
-        $order = $em->getRepository('P4MuseumBundle:Orders')->find($orderid); // récupération de la commande
+        $order = $em->getRepository('P4MuseumBundle:Orders')->find($id); // récupération de la commande
         $listtickets = $order->getTickets(); // récupération de la liste des tickets liés à la commande
         foreach($listtickets as $ticket)
         {
@@ -113,9 +120,35 @@ class TicketController extends Controller
 
         return $this->redirectToRoute('p4_museum_recap');
     }
-    public function confirmAction(Request $request)
+
+    public function confirmAction(Request $request) // Page de confirmation
     {
         $session = $request->getSession();
-        return $this->render('P4MuseumBundle:Ticket:confirm.html.twig');
+        $totalprice = $session->get('totalprice');
+        $id = $session->get('orderid');
+        $em = $this->getDoctrine()->getManager();
+        $order = $em->getRepository('P4MuseumBundle:Orders')->find($id); // récupération de la commande
+        $numberoftickets = $em->getRepository('P4MuseumBundle:Ticket')->countByOrder($id);
+
+        if($numberoftickets == 0){
+            throw new NotFoundHttpException();
+            $session->clear();
+        }
+
+        else {
+            if($totalprice == 0)
+            {
+                // Envoi du mail
+                $this->get('museum.mail')->sendConfirmMessage($order);
+
+                $order->setPaymentstatus(1);
+                $em->persist($order);
+                $em->flush();
+
+                $session->clear();// Suppression de la session
+            }
+            $session->clear();
+            return $this->render('P4MuseumBundle:Ticket:confirm.html.twig');
+        }
     }
 }
